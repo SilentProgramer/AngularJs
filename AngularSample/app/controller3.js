55b78a4bf83134a5090ba7059fb4265c2ab83bd2
Hemant <hemantsinghsrm@gmail.com>
2016-01-21 12:54:51 +0530

    Added CodeAcademy Project on Angular

--- AngularSample/app/controller3.js
+++ AngularSample/app/controller3.js
@@ -18,3 +18,31 @@ myApp.controller('GrandChildController', ['$scope', function ($scope) {
     $scope.timeOfDay = 'evening';
     $scope.name = 'Gingerbread Baby';
 }]);
+
+
+
+//if you need to test a nested controller you must create the same scope hierarchy in your test that exists in the DOM:
+
+describe('state', function () {
+    var mainScope, childScope, grandChildScope;
+
+    beforeEach(module('myApp'));
+
+    beforeEach(inject(function ($rootScope, $controller) {
+        mainScope = $rootScope.$new();
+        $controller('MainController', { $scope: mainScope });
+        childScope = mainScope.$new();
+        $controller('ChildController', { $scope: childScope });
+        grandChildScope = childScope.$new();
+        $controller('GrandChildController', { $scope: grandChildScope });
+    }));
+
+    it('should have over and selected', function () {
+        expect(mainScope.timeOfDay).toBe('morning');
+        expect(mainScope.name).toBe('Nikki');
+        expect(childScope.timeOfDay).toBe('morning');
+        expect(childScope.name).toBe('Mattie');
+        expect(grandChildScope.timeOfDay).toBe('evening');
+        expect(grandChildScope.name).toBe('Gingerbread Baby');
+    });
+});
---
+++ CodeAcademy.AngularJs/js/controllers/MainController.js
@@ -0,0 +1,10 @@
+﻿app.controller('MainController', ['$scope', function ($scope) {
+    $scope.title = 'Top Sellers of Books';
+    $scope.promo = 'Promotional Offers';
+
+    $scope.product = {
+        name: 'The Book of Trees',
+        price: 19
+    };
+
+}]);
