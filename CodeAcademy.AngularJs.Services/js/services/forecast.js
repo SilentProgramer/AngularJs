c7e015131c064732fec2dd8e6fdf188b702f4311
Hemant <hemantsinghsrm@gmail.com>
2016-01-25 20:54:00 +0530

    Finished weather app

--- CodeAcademy.AngularJs.Services/js/services/forecast.js
+++ CodeAcademy.AngularJs.Services/js/services/forecast.js
@@ -1,9 +1,16 @@
 ﻿app.factory('forecast', ['$http', function ($http) {
-    return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json')
+    var url = 'https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json';
+    return $http.get(url)
                 .success(function (data) {
                     return data;
                 })
                 .error(function (err) {
                     return err;
                 });
+
+    //if your internet connection is not working, you can use the below json file and images inside the "offline" folder 
+    //and host it on a service on a local server using nodejs or asp.net
+    //In the json file, make sure to change the icon property's value to the url of the image on the hosted local server
+
+
 }]);
---
+++ CodeAcademy.AngularJs.Services/offline/README.txt
@@ -0,0 +1,3 @@
+﻿The Offline folder holds the images and json file that the service usually returns.
+You can copy the contents of this folder and host them on a local server (using nodejs/asp.net) in case the internet is not working or the codeacademy link stops working.
+Other than that, this folder serves no purpose in this project
